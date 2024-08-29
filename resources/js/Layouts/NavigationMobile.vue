5271205fa93df3818a326e7520efba1e1328451c
Nestor Ramos <nestorsramosarteaga@gmail.com>
2024-08-29 17:56:38 -0500

    Create  models, Controllers and Migrations

---
+++ resources/js/Layouts/NavigationMobile.vue
@@ -0,0 +1,101 @@
+<template>
+  <transition
+      enter-active-class="transition ease-in-out duration-150"
+      enter-from-class="opacity-0"
+      enter-to-class="opacity-100"
+      leave-active-class="transition ease-in-out duration-150"
+      leave-from-class="opacity-100"
+      leave-to-class="opacity-0">
+    <div v-show="$page.props.showingMobileMenu" class="fixed inset-0 z-10 flex items-end bg-black bg-opacity-50 sm:items-center sm:justify-center"></div>
+  </transition>
+  <transition
+      enter-active-class="transition ease-in-out duration-150"
+      enter-from-class="opacity-0 transform -translate-x-20"
+      enter-to-class="opacity-100"
+      leave-active-class="transition ease-in-out duration-150"
+      leave-from-class="opacity-100"
+      leave-to-class="opacity-0 transform -translate-x-20">
+    <aside v-show="$page.props.showingMobileMenu" class="fixed inset-y-0 z-20 flex-shrink-0 w-64 mt-16 overflow-y-auto bg-purple-900 md:hidden">
+      <div class="py-4 text-gray-100">
+        <Link class="ml-6 text-lg font-bold text-gray-100" :href="route('dashboard')">
+        Library
+      </Link>
+        <ul class="mt-6">
+          <li class="relative px-6 py-3">
+            <ResponsiveNavLink :href="route('dashboard')" :active="route().current('dashboard')">
+              <template #icon>
+                <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round"
+                     stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                  <path
+                      d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path>
+                </svg>
+              </template>
+              Dashboard
+            </ResponsiveNavLink>
+          </li>
+
+          <li class="relative px-6 py-3">
+            <ResponsiveNavLink :href="route('users.index')" :active="route().current('users.index')">
+              <template #icon>
+                <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                     xmlns="http://www.w3.org/2000/svg">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path>
+                </svg>
+              </template>
+              Users
+            </ResponsiveNavLink>
+          </li>
+
+          <li class="relative px-6 py-3">
+            <ResponsiveNavLink :href="route('about')" :active="route().current('about')">
+              <template #icon>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" 
+                    d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
+                </svg>
+              </template>
+              Authors
+            </ResponsiveNavLink>
+          </li>
+
+        <li class="relative px-6 py-3">
+          <button @click="showingTwoLevelMenu = !showingTwoLevelMenu"
+              class="inline-flex items-center justify-between w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800"
+              aria-haspopup="true">
+                <span class="inline-flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                      <path stroke-linecap="round" stroke-linejoin="round" 
+                        d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
+                    </svg>
+                    <span class="ml-4">Books</span>
+                </span>
+            <svg class="w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+              <path fill-rule="evenodd"
+                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                    clip-rule="evenodd"></path>
+            </svg>
+          </button>
+            <ul v-show="showingTwoLevelMenu" class="p-2 mt-2 space-y-2 overflow-hidden text-sm font-medium text-gray-500 rounded-md shadow-inner bg-gray-50"
+                aria-label="submenu">
+              <li class="px-2 py-1 transition-colors duration-150 hover:text-gray-800">
+                <a class="w-full" href="#">List</a>
+              </li>
+              <li class="px-2 py-1 transition-colors duration-150 hover:text-gray-800">
+                <a class="w-full" href="#">Create</a>
+              </li>
+            </ul>
+        </li>
+        </ul>
+      </div>
+    </aside>
+  </transition>
+</template>
+
+<script setup>
+import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue'
+import { Link } from '@inertiajs/vue3';
+import { ref } from 'vue'
+
+let showingTwoLevelMenu = ref(false)
+</script>
