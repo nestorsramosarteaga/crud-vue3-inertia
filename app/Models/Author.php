bfad062a56813d91b2e3ba3ea4f8232ae3881b8b
Nestor Ramos <nestorsramosarteaga@gmail.com>
2024-08-30 06:54:13 -0500

    Update Models

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Author extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name','last_name','country_id'];
+
+    public function books(): BelongsToMany {
+        return $this->belongsToMany(Book::class);
+    }
+
+    public function country(): BelongsTo {
+        return $this->belongsTo(Country::class);
+    }
+    
 }
--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Book extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'isbn',
+        'title',
+        'description',
+        'publisher',
+        'release_date',
+        'pages',
+        'image'
+    ];
+
+    public function authors() : BelongsToMany {
+        return $this->belongsToMany(Author::class);
+    }
+    
 }
